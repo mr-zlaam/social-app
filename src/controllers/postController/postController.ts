84b3eaebc84f373905765a6e315fc649ac47044b
Zlaam <mrzalaam@gmail.com>
2024-06-13 11:45:24 +0500

    "Updated Prisma schema and added CreatePost function to postController"

--- src/controllers/postController/postController.ts
+++ src/controllers/postController/postController.ts
@@ -1,6 +1,14 @@
 import type { Request, Response } from "express";
 import { asyncHandler } from "../../utils/AsyncHandler";
+import { prisma } from "../../db";
 
-export const CreatePost = asyncHandler(
-  async (req: Request, res: Response) => {}
-);
+export const CreatePost = asyncHandler(async (req: Request, res: Response) => {
+  const { title, content, authorId } = req.body;
+  if (!title || !content || !authorId)
+    throw { status: 400, message: "All fields are required!!" };
+  const post = await prisma.post.create({
+    data: { title, content, authorId },
+    include: { author: true, comments: true },
+  });
+  res.send("Hello world");
+});
